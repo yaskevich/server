862a2da63e01df5dae758f819fd5c94902e78b32
yaskevich <alyaxey.yaskevich@gmail.com>
2021-10-29 20:17:11 +0300

    get info from nginx configs on bound ports (node-ports.pl)

--- node-ports.pl
+++ node-ports.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/perl
-# use feature qw(say);
+use feature qw(say);
 
 my $info = `lsof -i -P | grep node`;
 
 my %hash  = ();
+
+my $max_len  = 0;
 
 for my $line(split(/\n/g, $info)) {
 	my @line_cols = split(/\s+/g, $line);
@@ -12,10 +14,41 @@ for my $line(split(/\n/g, $info)) {
 	my @path_cols  = split(/->\s+/, $path_info);
 	my $path = $path_cols[1];
 	chomp ($path);
+	
+	my $path_len = length $path;
+	
+	if ($path_len > $max_len) {
+		$max_len = $path_len;
+	}
+	
 	$port_info =~ tr/0-9//cd;
-	$hash{$port_info} = $path;
+	$hash{$port_info} = [$path];
 }
 
+my $nginx_dir  = "/etc/nginx/sites-enabled";
+opendir my $dir, $nginx_dir or die "Cannot open NGINX directory: $!";
+
+for my $host (readdir $dir) {
+	if ($host ne "." and $host ne "..") {
+		my $config;
+		open(my $fh, '<', $nginx_dir."/".$host) or die "cannot open host config file $filename";
+		{
+			local $/;
+			$config = <$fh>;
+		}
+		$config =~ m/^\s*proxy_pass\s+http\:\/\/127\.0\.0\.1\:(.*?)\;$/gm;
+		if ($1 and exists $hash{$1}) {
+			push (@{$hash{$1}}, $host);
+		}
+		close($fh);		
+	}
+}
+
+closedir $dir;
+
+$max_len += 10;
 foreach my $port (sort keys %hash) {
-    printf " %-45s %s\n", $hash{$port}, $port;
+	my @site_info = @{$hash{$port}};
+	my $proxy = (scalar @site_info > 1) ? $site_info[1]: '';
+    printf " %-${max_len}s %s %s\n", $site_info[0], $port, $proxy;
 }
